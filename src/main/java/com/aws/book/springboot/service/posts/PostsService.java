e481037d706cece2acf3f38115c472b9eca66ada
JeongInjin <snowwwwww@naver.com>
2021-10-27 07:09:20 +0900

    수정, 조회 기능 추가 //TODO : 테스트코드 작성해야함.

--- src/main/java/com/aws/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/aws/book/springboot/service/posts/PostsService.java
@@ -1,14 +1,17 @@
 package com.aws.book.springboot.service.posts;
 
+import com.aws.book.springboot.domain.posts.Posts;
 import com.aws.book.springboot.domain.posts.PostsRepository;
+import com.aws.book.springboot.web.dto.PostsResponseDto;
 import com.aws.book.springboot.web.dto.PostsSaveRequestDto;
+import com.aws.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
-@RestController
+@Service
 public class PostsService {
 
     private final PostsRepository postsRepository;
@@ -16,6 +19,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다.id=" + id));
+        return new PostsResponseDto(entity);
     }
 
 
