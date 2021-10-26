b232773a30830c8e13eec53007f092ca1a2e04c2
JeongInjin <snowwwwww@naver.com>
2021-10-26 22:05:30 +0900

    퇴근후..Posts 서비스 등록 및 테스트코드 작성 너무 졸려서 오늘은 여기까지 내일아침에 다시..

---
+++ src/main/java/com/aws/book/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.aws.book.springboot.web;
+
+import com.aws.book.springboot.service.posts.PostsService;
+import com.aws.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
