b5884a8d939de8ca2fefc81f42ed7a42669108f9
JeongInjin <snowwwwww@naver.com>
2021-10-30 13:37:24 +0900

    네이버 로그인 추가.

--- src/main/java/com/aws/book/springboot/config/oauth/dto/SessionUser.java
+++ src/main/java/com/aws/book/springboot/config/oauth/dto/SessionUser.java
@@ -3,13 +3,15 @@ package com.aws.book.springboot.config.oauth.dto;
 import com.aws.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
 
-    public SessionUser(User user){
+    public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
