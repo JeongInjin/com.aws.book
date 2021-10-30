921ff5974c615a25cc595ccd98cfa921e37292d8
JeongInjin <snowwwwww@naver.com>
2021-10-30 11:18:27 +0900

    Spring SecurityConfig 추가 및 구글로그인 추가, ps.글쓰기 할때 권한 전부 풀었음.

--- src/main/java/com/aws/book/springboot/web/IndexController.java
+++ src/main/java/com/aws/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.aws.book.springboot.web;
 
+import com.aws.book.springboot.config.oauth.dto.SessionUser;
 import com.aws.book.springboot.service.posts.PostsService;
 import com.aws.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+
+        if(sessionUser != null){
+            model.addAttribute("userName", sessionUser.getName());
+        }
+
         return "index";
     }
 
