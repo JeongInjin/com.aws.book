c28ac18acec33e0e0af7d7193f2cb73fefcac72f
JeongInjin <snowwwwww@naver.com>
2021-11-11 07:14:14 +0900

    nginx 설정때문에 재배포..

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=jeong-springboot-webservice
 
-echo "> Build 파일 복사" 
+echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
@@ -16,7 +16,7 @@ CURRENT_PID=$(pgrep -fl jeong-springboot-webservice | grep jar | awk '{print $1}
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다. "
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
